21968a92b4b5b4804e7508c34d6dcad5c0e77dcb
LilMike0202 <giabaotong0202@gmail.com>
2026-06-11 05:27:21 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,51 +1,10 @@
-const SUPABASE_URL = “https://dkvorrbhaimxkrxslppd.supabase.co”;
-const SUPABASE_KEY = “sb_publishable_L7TOZSIrwCNSv-OLoJ2o7A_mTtxA2ts”;
-const { createClient } = supabase;
+const SUPABASE_URL = "https://dkvorrbhaimxkrxslppd.supabase.co";
+const SUPABASE_KEY = "sb_publishable_L7TOZSIrwCNSv-OLoJ2o7A_mTtxA2ts";
 
-const supabaseClient = createClient(
-SUPABASE_URL,
-SUPABASE_KEY
-);
+alert("auth.js loaded");
 
-const loginBtn = document.getElementById(“login-btn”);
-const logoutBtn = document.getElementById(“logout-btn”);
-const userInfo = document.getElementById(“user-info”);
+const loginBtn = document.getElementById("login-btn");
 
-// Đăng nhập bằng GitHub
-loginBtn.addEventListener("click", async () => {
-  alert("Button clicked!");
-
-  const { data, error } = await supabaseClient.auth.signInWithOAuth({
-    provider: "github"
-  });
-
-  if (error) {
-    alert(error.message);
-  }
+loginBtn.addEventListener("click", () => {
+    alert("Button clicked!");
 });
-// Đăng xuất
-logoutBtn.addEventListener(“click”, async () => {
-await supabaseClient.auth.signOut();
-location.reload();
-});
-
-// Kiểm tra trạng thái đăng nhập
-async function checkUser() {
-const {
-data: { user }
-} = await supabaseClient.auth.getUser();
-
-if (user) {
-userInfo.innerHTML =
-“👋 Hello, “ +
-(user.user_metadata.user_name || user.email);
-  loginBtn.style.display = "none";
-logoutBtn.style.display = "inline-block";
-} else {
-userInfo.innerHTML = “You are not logged in.”;
-loginBtn.style.display = "inline-block";
-logoutBtn.style.display = "none"; 
-  }
-}
-
-checkUser();
