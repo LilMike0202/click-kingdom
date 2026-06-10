7ff1b5650f0bd94f4f0043ff1aaf723a509776a4
LilMike0202 <giabaotong0202@gmail.com>
2026-06-11 05:37:30 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,23 +1,24 @@
-const SUPABASE_URL = "https://dkvorrbhaimxkrxslppd.supabase.co";
-const SUPABASE_KEY = "sb_publishable_L7TOZSIrwCNSv-OLoJ2o7A_mTtxA2ts";
+const SUPABASE_URL = “https://dkvorrbhaimxkrxslppd.supabase.co”;
+const SUPABASE_KEY = “sb_publishable_L7TOZSIrwCNSv-OLoJ2o7A_mTtxA2ts”;
 
-alert("auth.js loaded");
+const supabaseClient = supabase.createClient(
+SUPABASE_URL,
+SUPABASE_KEY
+);
 
-const loginBtn = document.getElementById("login-btn");
+const loginBtn = document.getElementById(“login-btn”);
 
-loginBtn.addEventListener("click", () => {
-    alert("Button clicked!");
+loginBtn.addEventListener(“click”, async () => {
+alert(“Button clicked!”);
+
+const { data, error } = await supabaseClient.auth.signInWithOAuth({
+provider: “github”,
+options: {
+redirectTo: “https://click-kingdom.vercel.app”
+}
 });
-//Đăng nhập bằng Github
-loginBtn.addEventListener("click", async () => {
-  const { data, error } = await supabaseClient.auth.signInWithOAuth({
-    provider: "github",
-    options: {
-      redirectTo: "https://click-kingdom.vercel.app"
-    }
-  });
 
-  if (error) {
-    alert(error.message);
-  }
+if (error) {
+alert(error.message);
+}
 });
