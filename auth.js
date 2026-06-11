8cb16d35323475409112a9992dc033fc6836444c
LilMike0202 <giabaotong0202@gmail.com>
2026-06-11 14:18:33 +0700

    Refactor supabaseClient initialization and cleanup
    Simplified supabaseClient initialization and removed login button event listener.

--- auth.js
+++ auth.js
@@ -1,27 +1,6 @@
 const SUPABASE_URL = "https://dkvorrbhaimxkrxslppd.supabase.co";
 const SUPABASE_KEY = "sb_publishable_L7TOZSIrwCNSv-OLoJ2o7A_mTtxA2ts";
 
-const supabaseClient = supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
-);
-
-const loginBtn = document.getElementById("login-btn");
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 console.log("auth.js loaded");
-
-loginBtn.addEventListener("click", async () => {
-  console.log("button clicked");
-
-  const { data, error } = await supabaseClient.auth.signInWithOAuth({
-    provider: "github",
-    options: {
-      redirectTo: "https://click-kingdom.vercel.app"
-    }
-  });
-
-  if (error) {
-    alert(error.message);
-    console.error(error);
-  }
-});
